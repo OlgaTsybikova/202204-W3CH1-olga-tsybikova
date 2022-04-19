751bd5be04b2341b7d3a8692ece72dd25337b860
Olga Tsybikova <tsy2701@gmail.com>
2022-04-20 00:19:07 +0200

    Added cards with character features

--- js/CardComponents/Componente.js
+++ js/CardComponents/Componente.js
@@ -1,10 +1,11 @@
 class Component {
   element;
 
-  constructor(parentElement, tag, className) {
-    this.element = document.createElement(tag);
-    this.element.className = className;
-    parentElement.appendChild(this.element);
+  constructor() {
+    this.element = document.createElement("li");
+    this.element.className = "character col";
+    document.querySelector(".characters-list").append(this.element);
   }
 }
+
 export default Component;
